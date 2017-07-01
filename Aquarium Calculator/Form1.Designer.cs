a81a821672a027b8130b75610aff75bbb5d9d4d2
Garrett <Garrettdeblois13@yahoo.com>
2017-07-01 11:12:03 -0500

    Added a copy to clipboard feature and also changed a few variables around and added more comments, added a "how to" (tutorial) button in the program as well as a "info" (credits) button.

--- Aquarium Calculator/Form1.Designer.cs
+++ Aquarium Calculator/Form1.Designer.cs
@@ -69,6 +69,7 @@
             this.label11 = new System.Windows.Forms.Label();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
@@ -77,7 +78,7 @@
             this.button1.Location = new System.Drawing.Point(285, 9);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(136, 166);
-            this.button1.TabIndex = 0;
+            this.button1.TabIndex = 4;
             this.button1.Text = "Calculate \r\nTotal";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
@@ -99,7 +100,7 @@
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(111, 48);
-            this.textBox1.TabIndex = 4;
+            this.textBox1.TabIndex = 1;
             // 
             // button2
             // 
@@ -107,7 +108,7 @@
             this.button2.Location = new System.Drawing.Point(16, 115);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(247, 60);
-            this.button2.TabIndex = 5;
+            this.button2.TabIndex = 3;
             this.button2.Text = "Submit Bucket";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
@@ -473,24 +474,38 @@
             this.textBox2.Multiline = true;
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(111, 48);
-            this.textBox2.TabIndex = 4;
+            this.textBox2.TabIndex = 2;
             // 
             // button3
             // 
             this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
-            this.button3.Location = new System.Drawing.Point(362, 190);
+            this.button3.Location = new System.Drawing.Point(372, 190);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(59, 29);
-            this.button3.TabIndex = 12;
+            this.button3.Size = new System.Drawing.Size(49, 29);
+            this.button3.TabIndex = 101;
+            this.button3.TabStop = false;
             this.button3.Text = "Info";
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.5F);
+            this.button4.Location = new System.Drawing.Point(285, 190);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(81, 29);
+            this.button4.TabIndex = 100;
+            this.button4.TabStop = false;
+            this.button4.Text = "How To";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(425, 256);
+            this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.label11);
             this.Controls.Add(this.label1);
@@ -582,6 +597,7 @@
         private System.Windows.Forms.Label label11;
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
     }
 }
 
--- Aquarium Calculator/Form1.cs
+++ Aquarium Calculator/Form1.cs
@@ -13,11 +13,26 @@ namespace Aquarium_Calculator
     public partial class Form1 : Form
     {
         decimal[] inputArray = new decimal[11];//has 11 items because it starts at 1 and skips 0
-        int counter = 0;//to increment through the array each time the user enters a new value in the text box      
+        int counter = 0;//to increment through the array each time the user enters a new value in the text box  
+        decimal initialResult = 0; //the amount of water in pounds (and then divided by the weight of what should be in the bucket, ex: 34 lbs / 41.7 lbs to give the ratio of weight in the bucket to what the weight in the bucket should be for it to be for 5, 10, etc amount of gallons depending on how many buckets the user enters)
+        decimal secondResult = 0; //the amount of water in gallons. this variable is used if the user copies the information to their clipboard
+        decimal finalResult = 0; //final answer, the percentage of water changed in the aquarium
 
         public Form1()
         {
             InitializeComponent();
+        }
+
+        void resultMessage()
+        {
+            MessageBox.Show("Your water change amount is " + Math.Round((finalResult * 100), 2).ToString() + "% of your aquarium water.", "Result");
+            DialogResult dialog = MessageBox.Show("Would you like to copy this information to clipboard?", "Result", MessageBoxButtons.YesNo);
+            if (dialog == DialogResult.Yes)
+            {
+                MessageBox.Show("Information copied to clipboard! Use either ctrl+V or right-click and \"paste\" to use this information elsewhere.", "Result");
+                String Today = DateTime.Today.ToString("d");
+                Clipboard.SetText("Date: " + Today + "      Water Change Percent: " + Math.Round((finalResult * 100), 2) + "%" + "       Water Change Amount: " + Math.Round((secondResult), 2) + " Gallons");
+            }
         }
 
         private void button2_Click(object sender, EventArgs e)
@@ -72,76 +87,82 @@ namespace Aquarium_Calculator
         private void button1_Click(object sender, EventArgs e)
         {
             //final calculation    
-            decimal aquariumSize = Convert.ToDecimal(textBox2.Text); //the number of gallons the aquarium is
-            decimal result = 0;
+            decimal aquariumSize = Convert.ToDecimal(textBox2.Text); //the number of gallons the aquarium is           
             switch (counter)
             {
                 case 1:
-                    result = inputArray[1] / Convert.ToDecimal(totalWeight1.Text);
-                    result = result * 5; //multiply the number of gallons that SHOULD be in the (5 gallon) bucket to give how many gallons you actually have based on the percent of weight (you have) to total weight (should be) of the bucket
-                    result = result / aquariumSize;
-                    MessageBox.Show("Your water change amount is " + Math.Round((result * 100), 2).ToString() + "% of your aquarium water.", "Result");
+                    initialResult = inputArray[1] / Convert.ToDecimal(totalWeight1.Text);
+                    secondResult = initialResult * 5; //multiply the number of gallons that SHOULD be in the (5 gallon) bucket to give how many gallons you actually have based on the percent of weight (you have) to total weight (should be) of the bucket
+                    finalResult = secondResult / aquariumSize;
+                    resultMessage();
                     break;
                 case 2:
-                    result = (inputArray[1] + inputArray[2]) / Convert.ToDecimal(totalWeight2.Text);  //adding the weight of the buckets together and then dividing by what the weight should be
-                    result = result * 10; //multiply the number of gallons that SHOULD be in the (5 gallon) bucket to give how many gallons you actually have based on the percent of weight (you have) to total weight (should be) of the bucket                    
-                    result = result / aquariumSize;
-                    MessageBox.Show("Your water change amount is " + Math.Round((result * 100), 2).ToString() + "% of your aquarium water.", "Result");
+                    initialResult = (inputArray[1] + inputArray[2]) / Convert.ToDecimal(totalWeight2.Text);  //adding the weight of the buckets together and then dividing by what the weight should be
+                    secondResult = initialResult * 10; //multiply the number of gallons that SHOULD be in the (5 gallon) bucket to give how many gallons you actually have based on the percent of weight (you have) to total weight (should be) of the buckets                    
+                    finalResult = secondResult / aquariumSize;
+                    resultMessage();
                     break;
                 case 3:
-                    result = (inputArray[1] + inputArray[2] + inputArray[3]) / Convert.ToDecimal(totalWeight3.Text);  //adding the weight of the buckets together and then dividing by what the weight should be
-                    result = result * 15; //multiply the number of gallons that SHOULD be in the (5 gallon) bucket to give how many gallons you actually have based on the percent of weight (you have) to total weight (should be) of the bucket                    
-                    result = result / aquariumSize;
-                    MessageBox.Show("Your water change amount is " + Math.Round((result * 100), 2).ToString() + "% of your aquarium water.", "Result");
+                    initialResult = (inputArray[1] + inputArray[2] + inputArray[3]) / Convert.ToDecimal(totalWeight3.Text);  //adding the weight of the buckets together and then dividing by what the weight should be
+                    secondResult = initialResult * 15; //multiply the number of gallons that SHOULD be in the (5 gallon) bucket to give how many gallons you actually have based on the percent of weight (you have) to total weight (should be) of the buckets                    
+                    finalResult = secondResult / aquariumSize;
+                    resultMessage();
                     break;
                 case 4:
-                    result = (inputArray[1] + inputArray[2] + inputArray[3] + inputArray[4]) / Convert.ToDecimal(totalWeight4.Text);  //adding the weight of the buckets together and then dividing by what the weight should be
-                    result = result * 20; //multiply the number of gallons that SHOULD be in the (5 gallon) bucket to give how many gallons you actually have based on the percent of weight (you have) to total weight (should be) of the bucket                    
-                    result = result / aquariumSize;
-                    MessageBox.Show("Your water change amount is " + Math.Round((result * 100), 2).ToString() + "% of your aquarium water.", "Result");
+                    initialResult = (inputArray[1] + inputArray[2] + inputArray[3] + inputArray[4]) / Convert.ToDecimal(totalWeight4.Text);  //adding the weight of the buckets together and then dividing by what the weight should be
+                    secondResult = initialResult * 20; //multiply the number of gallons that SHOULD be in the (5 gallon) bucket to give how many gallons you actually have based on the percent of weight (you have) to total weight (should be) of the buckets                    
+                    finalResult = secondResult / aquariumSize;
+                    resultMessage();
                     break;
                 case 5:
-                    result = (inputArray[1] + inputArray[2] + inputArray[3] + inputArray[4] + inputArray[5]) / Convert.ToDecimal(totalWeight5.Text);  //adding the weight of the buckets together and then dividing by what the weight should be
-                    result = result * 25; //multiply the number of gallons that SHOULD be in the (5 gallon) bucket to give how many gallons you actually have based on the percent of weight (you have) to total weight (should be) of the bucket                    
-                    result = result / aquariumSize;
-                    MessageBox.Show("Your water change amount is " + Math.Round((result * 100), 2).ToString() + "% of your aquarium water.", "Result");
+                    initialResult = (inputArray[1] + inputArray[2] + inputArray[3] + inputArray[4] + inputArray[5]) / Convert.ToDecimal(totalWeight5.Text);  //adding the weight of the buckets together and then dividing by what the weight should be
+                    secondResult = initialResult * 25; //multiply the number of gallons that SHOULD be in the (5 gallon) bucket to give how many gallons you actually have based on the percent of weight (you have) to total weight (should be) of the buckets                    
+                    finalResult = secondResult / aquariumSize;
+                    resultMessage();
                     break;
                 case 6:
-                    result = (inputArray[1] + inputArray[2] + inputArray[3] + inputArray[4] + inputArray[5] + inputArray[6]) / Convert.ToDecimal(totalWeight6.Text);  //adding the weight of the buckets together and then dividing by what the weight should be
-                    result = result * 30; //multiply the number of gallons that SHOULD be in the (5 gallon) bucket to give how many gallons you actually have based on the percent of weight (you have) to total weight (should be) of the bucket                    
-                    result = result / aquariumSize;
-                    MessageBox.Show("Your water change amount is " + Math.Round((result * 100), 2).ToString() + "% of your aquarium water.", "Result");
+                    initialResult = (inputArray[1] + inputArray[2] + inputArray[3] + inputArray[4] + inputArray[5] + inputArray[6]) / Convert.ToDecimal(totalWeight6.Text);  //adding the weight of the buckets together and then dividing by what the weight should be
+                    secondResult = initialResult * 30; //multiply the number of gallons that SHOULD be in the (5 gallon) bucket to give how many gallons you actually have based on the percent of weight (you have) to total weight (should be) of the buckets                    
+                    finalResult = secondResult / aquariumSize;
+                    resultMessage();
                     break;
                 case 7:
-                    result = (inputArray[1] + inputArray[2] + inputArray[3] + inputArray[4] + inputArray[5] + inputArray[6] + inputArray[7]) / Convert.ToDecimal(totalWeight7.Text);  //adding the weight of the buckets together and then dividing by what the weight should be
-                    result = result * 35; //multiply the number of gallons that SHOULD be in the (5 gallon) bucket to give how many gallons you actually have based on the percent of weight (you have) to total weight (should be) of the bucket                    
-                    result = result / aquariumSize;
-                    MessageBox.Show("Your water change amount is " + Math.Round((result * 100), 2).ToString() + "% of your aquarium water.", "Result");
+                    initialResult = (inputArray[1] + inputArray[2] + inputArray[3] + inputArray[4] + inputArray[5] + inputArray[6] + inputArray[7]) / Convert.ToDecimal(totalWeight7.Text);  //adding the weight of the buckets together and then dividing by what the weight should be
+                    secondResult = initialResult * 35; //multiply the number of gallons that SHOULD be in the (5 gallon) bucket to give how many gallons you actually have based on the percent of weight (you have) to total weight (should be) of the buckets                    
+                    finalResult = secondResult / aquariumSize;
+                    resultMessage();
                     break;
                 case 8:
-                    result = (inputArray[1] + inputArray[2] + inputArray[3] + inputArray[4] + inputArray[5] + inputArray[6] + inputArray[7] + inputArray[8]) / Convert.ToDecimal(totalWeight8.Text);  //adding the weight of the buckets together and then dividing by what the weight should be
-                    result = result * 40; //multiply the number of gallons that SHOULD be in the (5 gallon) bucket to give how many gallons you actually have based on the percent of weight (you have) to total weight (should be) of the bucket                    
-                    result = result / aquariumSize;
-                    MessageBox.Show("Your water change amount is " + Math.Round((result * 100), 2).ToString() + "% of your aquarium water.", "Result");
+                    initialResult = (inputArray[1] + inputArray[2] + inputArray[3] + inputArray[4] + inputArray[5] + inputArray[6] + inputArray[7] + inputArray[8]) / Convert.ToDecimal(totalWeight8.Text);  //adding the weight of the buckets together and then dividing by what the weight should be
+                    secondResult = initialResult * 40; //multiply the number of gallons that SHOULD be in the (5 gallon) bucket to give how many gallons you actually have based on the percent of weight (you have) to total weight (should be) of the buckets                    
+                    finalResult = secondResult / aquariumSize;
+                    resultMessage();
                     break;
                 case 9:
-                    result = (inputArray[1] + inputArray[2] + inputArray[3] + inputArray[4] + inputArray[5] + inputArray[6] + inputArray[7] + inputArray[8] + inputArray[9]) / Convert.ToDecimal(totalWeight9.Text);  //adding the weight of the buckets together and then dividing by what the weight should be
-                    result = result * 45; //multiply the number of gallons that SHOULD be in the (5 gallon) bucket to give how many gallons you actually have based on the percent of weight (you have) to total weight (should be) of the bucket                    
-                    result = result / aquariumSize;
-                    MessageBox.Show("Your water change amount is " + Math.Round((result * 100), 2).ToString() + "% of your aquarium water.", "Result");
+                    initialResult = (inputArray[1] + inputArray[2] + inputArray[3] + inputArray[4] + inputArray[5] + inputArray[6] + inputArray[7] + inputArray[8] + inputArray[9]) / Convert.ToDecimal(totalWeight9.Text);  //adding the weight of the buckets together and then dividing by what the weight should be
+                    secondResult = initialResult * 45; //multiply the number of gallons that SHOULD be in the (5 gallon) bucket to give how many gallons you actually have based on the percent of weight (you have) to total weight (should be) of the buckets                    
+                    finalResult = secondResult / aquariumSize;
+                    resultMessage();
                     break;
                 case 10:
-                    result = (inputArray[1] + inputArray[2] + inputArray[3] + inputArray[4] + inputArray[5] + inputArray[6] + inputArray[7] + inputArray[8] + inputArray[9] + inputArray[10]) / Convert.ToDecimal(totalWeight10.Text);  //adding the weight of the buckets together and then dividing by what the weight should be
-                    result = result * 50; //multiply the number of gallons that SHOULD be in the (5 gallon) bucket to give how many gallons you actually have based on the percent of weight (you have) to total weight (should be) of the bucket                    
-                    result = result / aquariumSize;
-                    MessageBox.Show("Your water change amount is " + Math.Round((result * 100), 2).ToString() + "% of your aquarium water.", "Result");
+                    initialResult = (inputArray[1] + inputArray[2] + inputArray[3] + inputArray[4] + inputArray[5] + inputArray[6] + inputArray[7] + inputArray[8] + inputArray[9] + inputArray[10]) / Convert.ToDecimal(totalWeight10.Text);  //adding the weight of the buckets together and then dividing by what the weight should be
+                    secondResult = initialResult * 50; //multiply the number of gallons that SHOULD be in the (5 gallon) bucket to give how many gallons you actually have based on the percent of weight (you have) to total weight (should be) of the buckets                    
+                    finalResult = secondResult / aquariumSize;
+                    resultMessage();
                     break;
             }
         }
 
-        private void button3_Click(object sender, EventArgs e)
+        private void button3_Click(object sender, EventArgs e) //credits
         {
-            //how to use program and credits, implement later
+            MessageBox.Show("Program was created by Garrett DeBlois, 2017", "Credits");
+        }
+
+        private void button4_Click(object sender, EventArgs e) //tutorial
+        {
+            MessageBox.Show("The left text box is for the weight of water in your bucket. If you have more than 1, five-gallon bucket, submit the first bucket and the form will dynamically increase in size to accommodate more buckets (up to a maximum of 10, five-gallon buckets).", "Tutorial Message 1 of 3");
+            MessageBox.Show("You only need to enter the total size of your aquarium(in gallons) one single time, at the end right before you click the \"Calculate Total\" button. This will add all the weights of your buckets together and find out how many gallons this is and then divide that number by your aquarium gallon size to find out what percent of your aquarium water you changed this time.", "Tutorial Message 2 of 3");
+            MessageBox.Show("If you would like to copy today's date, the water change percent, and the total amount of water (in gallons) you put in your aquarium this water change, click yes on the pop-up message at the end and you can then paste this information into a spreadsheet or other place so you can keep track of your aquarium water changes.", "Tutorial Message 3 of 3");
         }
     }
 }
